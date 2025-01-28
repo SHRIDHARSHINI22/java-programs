e5bdcad1792d493c3b0681da7a69c10c11ecc074
SHRIDHARSHINI22 <dharshinimshri@gmail.com>
2025-01-29 00:47:11 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Swap{
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+int a = sc.nextInt();
+int b = sc.nextInt();
+int temp =a;
+a=b;
+b=temp;
+System.out.println("After swapping:" +a);
+System.out.println("After swapping:" +b);
+}
+}
+
+
+
+
+
+
+
